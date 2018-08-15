87b660ac617d89a685c82b26f0c332f820e09d2a
r.garg2@student.unimelb.edu.au <rishabgarg.2021@gmail.com>
2018-08-15 14:03:48 +1000

    initialize

---
+++ start.py
@@ -0,0 +1,48 @@
+from fmclient import Agent
+from fmclient import Order, OrderType, OrderSide, Holding
+
+class Bot(Agent):
+
+
+
+    def __init__(self, account, email, password, marketplace_id):
+        name = "Simple_Bot"
+        super().__init__(account,email,password,marketplace_id,name=name)
+        self.description = "A bot learning to interact!"
+
+
+    def initialised(self):
+        pass
+
+    def order_accepted(self, order):
+        pass
+
+    def order_rejected(self, info, order):
+        pass
+
+    def received_order_book(self, order_book, market_id):
+        pass
+
+    def received_completed_orders(self, orders, market_id=None):
+        pass
+
+    def received_holdings(self, holdings):
+        pass
+
+    def received_marketplace_info(self, marketplace_info):
+        session_id= marketplace_info['session_id']
+        if marketplace_info['status']:
+            print("market place is active with session id = " + str(session_id))
+        else:
+            print("market is closed right now")
+
+    def run(self):
+        self.initialise()
+        self.start()
+
+
+
+if __name__ == "__main__":
+    marketplace_id = 352
+    fm_bot = Bot("bullish-delight", "r.garg2@student.unimelb.edu.au", "796799", marketplace_id)
+    fm_bot.run()
